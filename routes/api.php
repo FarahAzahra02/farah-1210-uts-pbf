0e9f9014fbb896ab29e6117806bac6787964bc46
FarahAzahra02 <farahazahra2002@gmail.com>
2024-05-20 22:32:15 +0700

    upload uts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriesCT;
+use App\Http\Controllers\ProductCT;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoogleAuthCT;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +17,28 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-//panggil ProductController sebagai objek
-// use App\Http\Controllers\ProductController;
+Route::post('/product',[ProductCT::class, 'store']);
+Route::get('/product',[ProductCT::class, 'showAll']);
+Route::get('/product/{id}',[ProductCT::class, 'showById']);
+Route::get('/product/search/name={name}',[ProductCT::class, 'showByName']);
+Route::put('product/{id}', [ProductCT::class, 'update']);
+Route::delete('product/{id}', [ProductCT::class,'delete']);
+Route::get('product/{id}/restore', [ProductCT::class, 'restore']);
 
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+Route::middleware(['web'])->group(function () {
+    Route::get('auth/google', [GoogleAuthCT::class, 'redirect']);
+    Route::get('auth/google/call-back', [GoogleAuthCT::class, 'callbackGoogle']);
+});
 
-Route::get('/product', [ProductController::class,'showAll']);
+Route::middleware(['jwt-auth'])->group(function(){
+    Route::post('/categories',[CategoriesCT::class, 'store']);
+    Route::get('/categories',[CategoriesCT::class, 'showAll']);
+    Route::get('/categories/{id}',[CategoriesCT::class, 'showById']);
+    Route::get('/categories/search/name={name}',[CategoriesCT::class, 'showByName']);
+    Route::put('categories/{id}', [CategoriesCT::class, 'update']);
+    Route::delete('categories/{id}', [CategoriesCT::class,'delete']);
+    Route::get('categories/{id}/restore', [CategoriesCT::class, 'restore']);
+});
